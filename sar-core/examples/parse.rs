e2cb7498473ff79e5b2fc3ab988437b168041d07
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-04-24 09:20:38 +0900

    write file

--- sar-core/examples/parse.rs
+++ sar-core/examples/parse.rs
@@ -3,4 +3,5 @@ fn main() {
     let sar = sar_parser_core::parser::payload::parse(Vec::from(file).into()).unwrap();
 
     println!("{:?}", sar);
+    std::fs::write("./sar-core/examples/result.txt", format!("{:?}", sar)).unwrap();
 }
