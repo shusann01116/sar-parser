fa02813f38dc252f768c325c255d444d840211e5
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-04-25 13:02:29 +0900

    wip

--- sar-core/src/renderer/transform.rs
+++ sar-core/src/renderer/transform.rs
@@ -1,8 +1,20 @@
+use image::{ImageBuffer, Rgba};
+
+use crate::core::sa::SymbolArt;
+
+struct Canvas {}
+
+// trait Drawer {
+//     type Item = SymbolArt;
+
+//     fn draw(sa: Item) -> ImageBuffer<Rgba<u8>, u8>;
+// }
+
 #[cfg(test)]
 mod tests {
-    use std::{sync::mpsc, time::Instant};
+    use std::time::Instant;
 
-    use image::{imageops, DynamicImage, GenericImage, RgbaImage};
+    use image::{imageops, GenericImage, RgbaImage};
     use rayon::prelude::*;
 
     use crate::{
@@ -10,26 +22,6 @@ mod tests {
         parser,
         test::RAW_FILE,
     };
-
-    #[test]
-    fn test_image_proc_projection() {
-        let bytes = include_bytes!("../../assets/symbols_b.png");
-        let image = image::load_from_memory(bytes).unwrap();
-
-        let projection = imageproc::geometric_transformations::Projection::from_control_points(
-            [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)],
-            [(0.0, 0.0), (0.2, 1.0), (0.8, 0.0), (1.0, 1.0)],
-        )
-        .unwrap();
-        let transformed = imageproc::geometric_transformations::warp(
-            &image.to_rgba8(),
-            &projection,
-            imageproc::geometric_transformations::Interpolation::Bilinear,
-            image::Rgba([0; 4]),
-        );
-
-        transformed.save("test.png").unwrap();
-    }
 
     #[test]
     fn test_projection_from_layer() {
@@ -44,48 +36,51 @@ mod tests {
         let mut result_image = RgbaImage::new(256, 256);
 
         let resource = crate::renderer::resource::Resource::new().unwrap();
-        let mut images = layers
-            .par_iter()
-            .rev()
-            .enumerate()
-            .filter_map(|(i, layer)| {
-                if layer.is_hidden() {
-                    return None;
+        let images = layers
+            .par_chunks(10)
+            .map(|chunk| {
+                let mut images = chunk
+                    .par_iter()
+                    .rev()
+                    .enumerate()
+                    .filter_map(|(i, layer)| {
+                        if layer.is_hidden() {
+                            return None;
+                        }
+
+                        let image = match resource.get_image(layer.symbol().id()) {
+                            Some(image) => image,
+                            None => {
+                                println!("Symbol {} not found", layer.symbol().id());
+                                return None;
+                            }
+                        };
+
+                        let mut new_image = RgbaImage::new(256, 256);
+                        imageproc::geometric_transformations::warp_into(
+                            &image.to_image(),
+                            &layer.try_into().unwrap(),
+                            imageproc::geometric_transformations::Interpolation::Bilinear,
+                            image::Rgba([0; 4]),
+                            &mut new_image,
+                        );
+
+                        Some((i, new_image))
+                    })
+                    .collect::<Vec<_>>();
+                images.par_sort_by_key(|(i, _)| *i);
+
+                let mut canvas = RgbaImage::new(256, 256);
+                for (_, img) in images {
+                    imageops::overlay(&mut canvas, &img, 0, 0);
                 }
 
-                let image = match resource.get_image(layer.symbol().id()) {
-                    Some(image) => image,
-                    None => {
-                        println!("Symbol {} not found", layer.symbol().id());
-                        return None;
-                    }
-                };
-
-                let mut new_image = RgbaImage::new(256, 256);
-                imageproc::geometric_transformations::warp_into(
-                    &image.to_image(),
-                    &layer.try_into().unwrap(),
-                    imageproc::geometric_transformations::Interpolation::Bilinear,
-                    image::Rgba([0; 4]),
-                    &mut new_image,
-                );
-
-                Some((i, new_image))
+                return canvas;
             })
             .collect::<Vec<_>>();
         println!("Sorted in {}ms", now.elapsed().as_millis());
 
-        let overlays = images
-            .par_chunks(10)
-            .map(|chunk| {
-                let mut image = RgbaImage::new(256, 256);
-                for (_, i) in chunk {
-                    imageops::overlay(&mut image, i, 0, 0);
-                }
-                image
-            })
-            .collect::<Vec<_>>();
-        for overlay in overlays {
+        for overlay in images {
             imageops::overlay(&mut result_image, &overlay, 0, 0);
         }
         println!("Overlayed in {}ms", now.elapsed().as_millis());
