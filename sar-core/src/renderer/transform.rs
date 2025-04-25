28c231af3761fd74366b0d0aa1c0dd9aa7341c77
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-04-25 09:46:36 +0900

    wip

--- sar-core/src/renderer/transform.rs
+++ sar-core/src/renderer/transform.rs
@@ -1,13 +1,13 @@
 #[cfg(test)]
 mod tests {
-    use std::result;
+    use std::{sync::mpsc, time::Instant};
 
-    use image::{imageops, DynamicImage};
+    use image::{imageops, DynamicImage, GenericImage, RgbaImage};
+    use rayon::prelude::*;
 
     use crate::{
         core::sa::{SymbolArt, SymbolArtLayer},
         parser,
-        renderer::resource,
         test::RAW_FILE,
     };
 
@@ -33,35 +33,69 @@ mod tests {
 
     #[test]
     fn test_projection_from_layer() {
+        let now = Instant::now();
+        println!("Started at {:?}", now);
+
         let bytes = Vec::from(RAW_FILE);
         let sa = parser::parse(bytes.into()).unwrap();
+        println!("Parsed in {}ms", now.elapsed().as_millis());
+
         let layers = sa.layers();
-        let mut result_image = DynamicImage::new(256, 256, image::ColorType::Rgba8);
-        for layer in layers.iter().rev() {
-            if layer.is_hidden() {
-                continue;
-            }
+        let mut result_image = RgbaImage::new(256, 256);
 
-            let resource = crate::renderer::resource::Resource::new().unwrap();
-
-            let image = match resource.get_image(layer.symbol().id()) {
-                Some(image) => image,
-                None => {
-                    println!("Symbol {} not found", layer.symbol().id());
-                    continue;
+        let resource = crate::renderer::resource::Resource::new().unwrap();
+        let mut images = layers
+            .par_iter()
+            .rev()
+            .enumerate()
+            .filter_map(|(i, layer)| {
+                if layer.is_hidden() {
+                    return None;
                 }
-            };
 
-            let mut new_image = DynamicImage::new(256, 256, image::ColorType::Rgba8).into();
-            imageproc::geometric_transformations::warp_into(
-                &image.to_image(),
-                &layer.try_into().unwrap(),
-                imageproc::geometric_transformations::Interpolation::Bilinear,
-                image::Rgba([0; 4]),
-                &mut new_image,
-            );
-            imageops::overlay(&mut result_image, &new_image, 0, 0);
+                let image = match resource.get_image(layer.symbol().id()) {
+                    Some(image) => image,
+                    None => {
+                        println!("Symbol {} not found", layer.symbol().id());
+                        return None;
+                    }
+                };
+
+                let mut new_image = RgbaImage::new(256, 256);
+                imageproc::geometric_transformations::warp_into(
+                    &image.to_image(),
+                    &layer.try_into().unwrap(),
+                    imageproc::geometric_transformations::Interpolation::Bilinear,
+                    image::Rgba([0; 4]),
+                    &mut new_image,
+                );
+
+                Some((i, new_image))
+            })
+            .collect::<Vec<_>>();
+        println!("Sorted in {}ms", now.elapsed().as_millis());
+
+        let overlays = images
+            .par_chunks(10)
+            .map(|chunk| {
+                let mut image = RgbaImage::new(256, 256);
+                for (_, i) in chunk {
+                    imageops::overlay(&mut image, i, 0, 0);
+                }
+                image
+            })
+            .collect::<Vec<_>>();
+        for overlay in overlays {
+            imageops::overlay(&mut result_image, &overlay, 0, 0);
         }
-        result_image.save(format!("test.png")).unwrap();
+        println!("Overlayed in {}ms", now.elapsed().as_millis());
+
+        result_image
+            .sub_image(128 - 96, 128 - 48, 193, 96)
+            .to_image()
+            .save(format!("test.png"))
+            .unwrap();
+
+        println!("Saved in {}ms", now.elapsed().as_millis());
     }
 }
